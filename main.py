947dec86ec453e0319241cb4fa0d3f69651f88e5
Lucasjkofcc <brawllucas15@gmail.com>
2026-06-05 20:59:41 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+number = random.randint(1, 100)
+while True:
+    guess = int(input("Guess a number between 1 and 100: "))
+    if guess < number:
+        print("Too low! Try again.")
+    elif guess > number:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! You've guessed the number!")
+        print("Restarting the game...")
+        number = random.randint(1, 100)
