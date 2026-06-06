df35f308215e85eba3d368ba0c3ecdd1c0d40f45
Lucasjkofcc <brawllucas15@gmail.com>
2026-06-05 21:02:05 -0300

    exit option added

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import random
 number = random.randint(1, 100)
+print("Digit any letter to exit!")
 while True:
-    guess = int(input("Guess a number between 1 and 100: "))
-    if guess < number:
-        print("Too low! Try again.")
-    elif guess > number:
-        print("Too high! Try again.")
-    else:
-        print("Congratulations! You've guessed the number!")
-        print("Restarting the game...")
-        number = random.randint(1, 100)
+    try:
+        guess = int(input("Guess a number between 1 and 100: "))
+        if guess < number:
+            print("Too low! Try again.")
+        elif guess > number:
+            print("Too high! Try again.")
+        else:
+            print("Congratulations! You've guessed the number!")
+            print("Restarting the game...")
+            number = random.randint(1, 100)
+    except ValueError:
+        print("Exited Successfully!")
+        break
